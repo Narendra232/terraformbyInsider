3eae2a34f79d449e015041d6941c826219f4f7a3
Narendra Kumar <56841745+Narendra232@users.noreply.github.com>
2026-05-26 22:47:44 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.69.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend"
+    storage_account_name = "devopsinsidersbackend"
+    container_name       = "backendcontainer"             
+    key                  = "backendcontainer.tfstate"     
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg-backendmango"
+  location = "West US"
+}
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "rg-backendorange"
+  location = "West US"
+}
+
+resource "azurerm_resource_group" "rg4" {
+  name     = "rg-backendavocardo"
+  location = "West US"
+}
+
+resource "azurerm_resource_group" "rg5" {
+  name     = "rg-backendbanana"
+  location = "West US"
+}
